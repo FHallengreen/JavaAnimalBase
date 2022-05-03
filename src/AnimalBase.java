6d5164ad0cc3db58db7a5dc1fd0af8156d001d1b
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-05-03 10:57:51 +0200

    added exception

--- src/AnimalBase.java
+++ src/AnimalBase.java
@@ -1,19 +1,17 @@
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Locale;
-import java.util.Scanner;
 
 public class AnimalBase {
 
     private ArrayList<Animal> animals;
+    FileHandler handler = new FileHandler();
 
     public AnimalBase() {
         animals = new ArrayList<>();
     }
 
     public void start() throws FileNotFoundException {
+        loadDatabase();
         UserInterface ui = new UserInterface(this);
         ui.start();
     }
@@ -37,14 +35,14 @@ public class AnimalBase {
     }
 
     public void createNewAnimal(String name, String description, String type, int age, double weight) {
-        Animal animal = new Animal(name,description,type,age,weight);
+        Animal animal = new Animal(name, description, type, age, weight);
         animals.add(animal);
     }
 
     public boolean deleteAnimal(String name) {
         // find animal with this name
         Animal animal = findAnimalByName(name);
-        if(animal == null) {
+        if (animal == null) {
             return false;
         } else {
             animals.remove(animal);
@@ -53,8 +51,8 @@ public class AnimalBase {
     }
 
     private Animal findAnimalByName(String name) {
-        for(Animal animal : animals) {
-            if(animal.getName().equalsIgnoreCase(name)) {
+        for (Animal animal : animals) {
+            if (animal.getName().equalsIgnoreCase(name)) {
                 return animal;
             }
         }
@@ -62,36 +60,17 @@ public class AnimalBase {
     }
 
 
-    public void loadDatabase() throws FileNotFoundException {
-       Scanner fileScanner = new Scanner(new File("animals.csv"));
-       while  (fileScanner.hasNextLine()){
-           String line = fileScanner.nextLine();
-           Scanner input = new Scanner(line).useDelimiter(";").useLocale(Locale.ENGLISH);
-           String name = input.next();
-           String description = input.next();
-           String type = input.next();
-           int age = input.nextInt();
-           double weight = input.nextDouble();
-           Animal animal = new Animal(name,description,type,age,weight);
-           animals.add(animal);
-       }
-    }
-
-    public void saveDatabase() throws FileNotFoundException {
-        PrintStream out = new PrintStream(new File("animals.csv"));
-        for (int i = 0; i < animals.size(); i++) {
-            Animal animal = animals.get(i);
-            out.print(animal.getName());
-            out.print(";");
-            out.print(animal.getDesc());
-            out.print(";");
-            out.print(animal.getType());
-            out.print(";");
-            out.print(animal.getAge());
-            out.print(";");
-            out.print(animal.getWeight());
-            out.print("\n");
+    public void loadDatabase() {
+        try {
+        handler.loadAnimalsToFile(animals);
+        }catch (FileNotFoundException e){
+            System.out.println(e);
         }
     }
 
+    public void saveDatabase() throws FileNotFoundException {
+
+        handler.saveAnimalsToFile(animals);
+
+    }
 }
---
+++ src/FileHandler.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class FileHandler {
+
+    public void loadAnimalsToFile(ArrayList<Animal> animals) throws FileNotFoundException {
+        Scanner fileScanner = new Scanner(new File("animalss.csv"));
+        while  (fileScanner.hasNextLine()){
+            String line = fileScanner.nextLine();
+            Scanner input = new Scanner(line).useDelimiter(";").useLocale(Locale.ENGLISH);
+            String name = input.next();
+            String description = input.next();
+            String type = input.next();
+            int age = input.nextInt();
+            double weight = input.nextDouble();
+            Animal animal = new Animal(name,description,type,age,weight);
+            animals.add(animal);
+        }
+    }
+
+    public void saveAnimalsToFile(ArrayList<Animal> animals) throws FileNotFoundException {
+        PrintStream out = new PrintStream(new File("animals.csv"));
+        for (int i = 0; i < animals.size(); i++) {
+            Animal animal = animals.get(i);
+            out.print(animal.getName());
+            out.print(";");
+            out.print(animal.getDesc());
+            out.print(";");
+            out.print(animal.getType());
+            out.print(";");
+            out.print(animal.getAge());
+            out.print(";");
+            out.print(animal.getWeight());
+            out.print("\n");
+        }
+    }
+}
